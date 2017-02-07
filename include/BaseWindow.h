ced2638ec2cd76be90da25a1c3c29485ea79b205
janvik <janvik@localhost>
2017-02-07 16:29:10 -0700

    Updating headers and Makefile

---
+++ include/BaseWindow.h
@@ -0,0 +1,22 @@
+#ifndef BASEWINDOW_H
+#define BASEWINDOW_H
+
+#include <Window.h>
+
+/**
+* This class provides a concrete implemenation for Window.
+*/
+class BaseWindow : public Window
+{
+    public:
+        BaseWindow(int w=10, int h=10);
+
+        virtual ~BaseWindow();
+
+        /**
+        * Method does nothing.
+        */
+        void draw(){};
+};
+
+#endif // BASEWINDOW_H
