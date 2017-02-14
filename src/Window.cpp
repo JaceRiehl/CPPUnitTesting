3c4fad76b59e271c6d8c9c858c07f55d05bbdb98
Jace Riehl <jace.riehl@gmail.com>
2017-02-13 19:47:41 -0700

    Added functionality to Window.cpp

--- src/Window.cpp
+++ src/Window.cpp
@@ -5,39 +5,44 @@
 #include <vector>
 
 
-    Window(int w=100, int h=100) : height(h), width(w){}
+    Window(int w=100, int h=100)
+    {
+        if(w > 1024 || h > 1024)
+            throw illegal_size_error("Window exceeds maximum size");
+        else
+        {
+            width = w;
+            height = h;
+        }
+    }
 
-    virtual ~Window()
+    Window::~Window()
     {
 
     }
 
-    Coordinate getLocation()
-    {
 
-    }
-
-    void addWidget(Widget* w)
+    void Window::addWidget(Widget* w)
     {
         contents.push_back(w);
     }
 
-    unsigned int getWidth()
+    unsigned int Window::getWidth()
     {
         return width;
     }
 
-    unsigned int getHeight()
+    unsigned int Window::getHeight()
     {
         return height;
     }
 
-    std::vector<Widget*> getWidgets()
+    std::vector<Widget*> Window::getWidgets()
     {
         return contents;
     }
 
-    void removeWidget(Widget* w)
+    void Window::removeWidget(Widget* w)
     {
         for(int i = 0; i < std::vector contents.size; i++)
         {
@@ -46,16 +51,29 @@
         }
     }
 
-    void resize(unsigned int w, unsigned int h)
+    void Window::resize(unsigned int w, unsigned int h)
     {
+        if((w || h) > 1024)
+            throw illegal_size_error("Window exceeds maximum size");
 
+        for(int i = 0; i < contents.size() + 1; i++)
+        {
+            if(w < (contents[i]->width + contents[i]->location.x) || h < (contents[i]->height + contents[i]->location.y))
+                throw illegal_size_error("Window cuts off a widget");
+        }
+
+        else
+        {
+            width = w;
+            height = h;
+        }
     }
 
-    void setHeight(int h)
+    void Window::setHeight(int h)
     {
          height = h;
     }
-    void setWidth(int w)
+    void Window::setWidth(int w)
     {
        width = w;
     }
